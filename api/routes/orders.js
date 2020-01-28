c17cce958c43d7091721c07985c079b0c24bc5d8
Marcus Edvardsson <54709946+jensengbg-marcus-edvardsson@users.noreply.github.com>
2020-01-28 21:56:55 +0100

    init
    init

---
+++ api/routes/orders.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Ordrar hämtade'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
+    res.status(201).json({
+        message: 'Ordrar skapade',
+        order: order
+
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order detaljer',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order delete',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router; 
---
+++ api/routes/products.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hanterar GET requests för /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price,
+        img: req.body.imgURL
+    }
+    res.status(201).json({
+        message: 'Hanterar POST requests för /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Detta är ett speciellt ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+          message: 'Du har fått ett ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Uppdaterade produkt'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Tog bort produkt'
+    });
+});
+
+module.exports = router; 
