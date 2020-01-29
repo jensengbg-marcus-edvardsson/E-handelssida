dd8d60e9bc8d7a168b56200a901235342acf6207
Marcus Edvardsson <marcusedvardsson@live.se>
2020-01-29 13:37:02 +0100

    edit
    edit

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,42 +10,4 @@ router.get('/', (req, res, next) => {
         res.send(products);
 });
 
-// router.post('/', (req, res, next) => {
-//     const product = {
-//         name: req.body.name,
-//         price: req.body.price,
-//         img: req.body.img
-//     }
-//     res.status(201).json({
-//         message: 'Hanterar POST requests för /products',
-//         createdProduct: product
-//     });
-// });
-
-// router.get('/:productId', (req, res, next) => {
-//     const id = req.params.productID;
-//     if (id === 'special') {
-//         res.status(200).json({
-//             message: 'Detta är ett speciellt ID',
-//             id: id
-//         });
-//     } else {
-//         res.status(200).json({
-//           message: 'Du har fått ett ID'
-//         });
-//     }
-// });
-
-// router.patch('/:productId', (req, res, next) => {
-//     res.status(200).json({
-//         message: 'Uppdatera produkt'
-//     });
-// });
-
-// router.delete('/:productId', (req, res, next) => {
-//     res.status(200).json({
-//         message: 'Ta bort produkt'
-//     });
-// });
-
 module.exports = router; 
